38675bc1a1684069e42b2f557724fe7dfcec3935
Ole Gligorov <olegligorov@gmail.com>
2023-05-02 11:07:42 +0300

    First commit

---
+++ src/main/java/com/imageclassification/ImageClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.imageclassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageClassificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imageclassification/ImageClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imageclassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageClassificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
